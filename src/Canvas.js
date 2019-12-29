2bf553814ccb2602ca28fec8aea53691c285d37e
Nikos Fasarakis Hilliard <nfasarakis@gmail.com>
2019-12-29 11:35:16 +0200

    - Add documentation - Add requestAnimationFrame from smoother animation - Fix bug with moving center in draw() method of image   instances

--- src/Canvas.js
+++ src/Canvas.js
@@ -17,11 +17,20 @@ const CANVAS_IMAGE_PROPS = [
 
 /**
  * Class for images drawn on screen
+ * Each instance holds all positional data & methods necessary to draw an image
+ * on the canvas using the drawImage() method from the canvas API
+ * See https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
  */
 class CanvasImage {
 
   /**
-   *
+   * @param {number} left - left corner in px of the image to be painted within the canvas
+                            expressed relative to the canvas origin
+   * @param {number} top -  top corner in px of the image to be painted within the canvas,
+   *                        expressed relative to the canvas dimensions
+   * @param {number} width - Width in px of the image to be painted within the canvas
+   * @param {number} height - Height in px of the imge to be painted within the canvas
+   * @param {string} url - string URL, image source
    */
   constructor(left, top, width, height, url) {
     this.width = width;
@@ -34,47 +43,57 @@ class CanvasImage {
   }
 
   /**
+   * Responsible for the initial drawing of the image on the canvas.
+   * Used in componentDidMount() lifecycle method
    *
+   * @param {object} ctx - See CANVAS API: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage
+   *                       A CanvasRenderingContext2D object, providing the draw functions
    */
   drawInit(ctx) {
-    // Create new img element
+    // Create new img element and draw it on the canvas when it loads
     this.img = new Image();
-    // Attach onload handler
     this.img.addEventListener('load', () => {
       ctx.drawImage(this.img, this.center.x - this.width / 2, this.center.y - this.height / 2, this.width, this.height);
     }, false);
-    // Set source
-    this.img.src = this.url; // Set source path
+    this.img.src = this.url;
 
-    // Draw rectangle center (for testing)
+    // Draw a rectangle at the center of the image (for testing)
     ctx.fillStyle = 'rgb(40, 0, 0)';
     ctx.fillRect(this.center.x-3, this.center.y-3, 6, 6);
   }
 
   /**
-   * Given previous & current mouse Pos, draw Rect on canvas
-   * Center moves linearly with mousePos by specific factors
-   * Width and height grow linearly as mouse approaches center of rectangle
+   * Responsible for drawing an new image on the canvas whenever the mouse moves
+   *
+   * To achieve a parallax effect, the image's center moves linearly in the opposite
+   *    direction of the mouse movement by a factor of 1/6 & 1/8 on the x and y axis resp.
+   * The width and height of the drawn image grow linearly as the mouse approaches the
+   *    center of the image
+   *
+   * @param {object} ctx - Canvas rendering context, providing the draw functions
+   * @param {object} prevMouseCoords - The last recorded position of the mouse on
+   *                                   the 2D canvas. Object containing x and y coords
+   * @param {object} mouseCoords - Current position of the mouse on the 2D canvas
+   *                               Object containing x and y coords.
    */
   draw(ctx, prevMouseCoords, mouseCoords) {
 
-    // Move new center in opposite direction of mouse movement
-    // Move by 1/6 * X difference and 1/8 Y difference in mouse position
+    // Parallax Effect: Move center of image in opposite direction of mouse movement
     if (prevMouseCoords.x !== 0 && prevMouseCoords.y !== 0) {
-      this.center.x = this.center.x - (mouseCoords.x - prevMouseCoords.x)/6;
-      this.center.y = this.center.y - (mouseCoords.y - prevMouseCoords.y)/8;
+      this.center.x = this.center.x + (mouseCoords.x - prevMouseCoords.x)/6;
+      this.center.y = this.center.y + (mouseCoords.y - prevMouseCoords.y)/8;
     }
 
-    // Get distance-related multiplication factor
-    // The closer the mouse Coords are to the center, the closer the factor to 1
+    // The closer the mouse is to the image's center, the larger the image dimensions
+    // This is achieved by computing a multiplication factor that
+    //    1) is 1 when the mouse coordinates become equal to the images center
+    //    2) decreases linearly as the distance between mouse and image center grows,
+    //       reaching zero when the distance becomes larger than 300px (cuttOffDistance)
     let mFactor = getMultiplicationFactor(300, this.center, mouseCoords)
-
-    // Draw rectangle (around it's center) with dimensions depending on how close
-    // the mouse is to said center using the mFactor
     let dynamicWidth = this.width + this.width * mFactor;
     let dynamicHeight = this.height + this.height * mFactor;
 
-    // Create new img element
+    // Draw a new image on the canvas around the new center w/ the new dimensions
     ctx.drawImage(this.img, this.center.x - dynamicWidth/2, this.center.y - dynamicHeight/2, dynamicWidth, dynamicHeight);
 
 
@@ -84,7 +103,7 @@ class CanvasImage {
   }
 
   /**
-   *
+   * Fires when region occupied by the image in the canvas is clicked
    */
   onClick() {
     alert(`clicked ${this.url}`);
@@ -98,7 +117,7 @@ class CanvasImage {
 export default class Canvas extends React.Component {
 
   /**
-   * Holds mouse coords from previous animation cycle
+   * Holds last recorded mouse coordinates in the 2D canvas
    */
   prevMouseCoords = {
     x: 0,
@@ -118,7 +137,7 @@ export default class Canvas extends React.Component {
   }
 
   /**
-   * Should set-up elements/shapes in canvas
+   * Sets-up images in canvas - Initial draw
    */
   componentDidMount() {
     // Draw function
@@ -136,7 +155,10 @@ export default class Canvas extends React.Component {
   }
 
   /**
+   * Retrieves mouse coordinates within canvas
    *
+   * @param {object} evt - Event object, retrieved on click
+   * @returns {object} Object containing x and y coords on mouse within the canvas
    */
   getMouseCoords = evt => {
     let canvasBox = this.canvas.getBoundingClientRect();
@@ -147,27 +169,37 @@ export default class Canvas extends React.Component {
   }
 
   /**
-   * Should perform animation
+   * MouseMove Event Handler
+   *
+   * Computes mouse position and uses it to re-draw images on canvas
+   * Each images position and dimension depend on said mouse position
+   * in a way that creates a parallax and scalling effect as determined
+   * in each image instance draw() menthod.
+   *
+   * @param {object} evt - Event object
    * REQUESTANIMATIONFRAME????
    */
   handleMouseMove = evt => {
     let ctx = this.canvas.getContext('2d');
     let mouseCoords = this.getMouseCoords(evt);
 
-    // Clear canvas
-    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    requestAnimationFrame(() => {
+      // Clear canvas
+      ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    // Order canvas elements bassed on their centers distance from mouseCoords
-    // Elements w/ centers closest to mouse Coords get placed at end of array
-    // This is so they are always painted on top of elements that are further away
-    this.canvasElements.sort((elem1, elem2) =>
-      euclideanDistance(elem2.center, mouseCoords) - euclideanDistance(elem1.center, mouseCoords)
-    )
+      // Order canvas elements bassed on the distance of their centers from mouseCoords
+      // Elements w/ centers closest to mouse Coords get placed at end of array
+      this.canvasElements.sort((elem1, elem2) =>
+        euclideanDistance(elem2.center, mouseCoords) - euclideanDistance(elem1.center, mouseCoords)
+      )
 
-    // Draw new rectangles
-    this.canvasElements.map(elem =>
-      elem.draw(ctx, this.prevMouseCoords, mouseCoords)
-    );
+      // Draw new images by mapping over canvas elements
+      // Due to sorting, elements closest to mouse coords get painted over elements
+      // that are furter away.
+      this.canvasElements.map(elem =>
+        elem.draw(ctx, this.prevMouseCoords, mouseCoords)
+      );
+    })
 
     // Store new mouse coords for use in next onMouseMove event
     this.prevMouseCoords = mouseCoords;
@@ -175,16 +207,23 @@ export default class Canvas extends React.Component {
   }
 
   /**
+   * Click Event Handler (on canvas)
    *
+   * Elements drawn on canvas don't fire click events.
+   * Therefore, a click is registered on the canvas and the region clicked
+   * is checked against the regions of all drawn elements.
+   * The closest matched element fires it's onClick() method
+   *
+   * @param {object} evt - Event object
    */
   handleCanvasClick = (evt) => {
-    // Find what element was clicked on
     let mouseCoords = this.getMouseCoords(evt);
 
-    // If mouse coords is withing images bounds, it's been clicked
-    // Loop over canvas elements
+    // Loop over all canvas elements and retrieve their regions
+    // If the mouse coords are withing image's region, it's been clicked
     let clickedElements = this.canvasElements.filter(elem => {
-      // When calculating bounds, also factor in mFactor
+      // When calculating the regions, also consider the mFactor.
+      // The mFactor used in draw() alters the image's dimensios, hence it's region
       let mFactor = getMultiplicationFactor(300, elem.center, mouseCoords);
       let dynamicWidth = elem.width + elem.width * mFactor;
       let dynamicHeight = elem.height + elem.height * mFactor;
@@ -195,9 +234,11 @@ export default class Canvas extends React.Component {
       mouseCoords.y < elem.center.y + dynamicHeight /2
     });
 
-    // For overlapping elements, execute click handler for the one whose
-    // center is clossest to the mouse coords
-    // clickedElements already sorted since this.canvasElements are sorted
+    // The clickedElements array may contain multiple entries as images may overlap in the canvas.
+    // Since filtered this.canvasElements array is ordered from farther to closest to the mouseCoords,
+    //     so is the clickedElements array.
+    // The last element of either array is the closest to the mouse coords, so consider
+    // last element of clickedElements as the one that's actually clicked.
     clickedElements.length > 0 && clickedElements[clickedElements.length - 1].onClick();
 
   }
--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,10 @@
 /**
+ * Calculates the Euclidean distance between two 2D points
  *
+ * @param {object} p1 - Object containing x and y coord values
+ * @param {object} p2 - Object containing x and y coord values
+ *
+ * @returns The Euclidean distance between points p1 and p2, ||p1-p2||
  */
 let euclideanDistance = (p1,p2) => {
     let xdiff = Math.pow((p1.x-p2.x),2);
@@ -8,16 +13,22 @@ let euclideanDistance = (p1,p2) => {
 }
 
 /**
- * returns number between [0,1] depending on how close p1 is to p2
- * Need to make this an exponential function
+ * Given tho points p1 and p2, returns a cooefficient that linearly
+ *    aproaches the value of 1 the closer p1 is to p2
+ *    aproaches the value of 0 as the distance of p1 & p2 aproaches the cuttOffDistance
+ * i.e f(||p1-p2||) = (-1/cutOffDistance) * ||p1 - p2|| + 1 for ||p1-p2|| <= cutOffDistance
+ *
+ * @param {object} p1 - Object containing x and y coord values
+ * @param {object} p2 - Object containing x and y coord values
+ * @param {number} cutOffDistance - Distance after which the
+ *                                  returned cooefficient is always zero
+ *
+ * @returns {number} Cooefficient/factor w/ value as specified above
  */
 let getMultiplicationFactor = (cutOffDistance, p1, p2) => {
-  // Calculate euclidean distance between center of rect and mouse mooseCoords
   let distance = euclideanDistance(p1, p2);
 
   if (distance < cutOffDistance) {
-    // Linear increase as centerX approaches mouseCoords.x as long as max difference is < cutOffDistance
-    // y = -1/max (centerX - mooseCoords.x) + 1 where max = cutOffDistance
     return ((-1 / cutOffDistance) * distance + 1);
   } else {
     return 0;
